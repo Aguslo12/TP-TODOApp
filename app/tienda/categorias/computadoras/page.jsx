4771dc3e6330853327bf5f853ad2ff7dfb0da08d
Agustin Lobos <aguslobos.1712@gmail.com>
2023-10-24 00:28:08 -0300

    first commit

---
+++ app/tienda/categorias/computadoras/page.jsx
@@ -0,0 +1,5 @@
+export default function ComputadorasPage(){
+    return(
+        <h1>Seccion computadoras</h1>
+    )
+}
---
+++ app/tienda/page.jsx
@@ -0,0 +1,10 @@
+export default function Tienda() {
+    return (
+        <div>
+            <h1>Tienda</h1> 
+            <ul>
+                <li>Ropa</li>
+            </ul>
+        </div>
+    )
+}
